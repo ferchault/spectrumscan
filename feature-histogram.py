53f44c28aac18ceac1749c2718c1050452f678bf
Guido <guido@vonrudorff.de>
2020-06-24 12:14:33 +0200

    EAV output #3

--- feature-histogram.py
+++ feature-histogram.py
@@ -8,4 +8,7 @@ def read_file():
 if __name__ == "__main__":
 	q = read_file()
 	for col in range(12):
-		print (col, ' '.join(map(str, np.bincount(q[:, col]))))
+		counts = np.bincount(q[:, col])
+		for i in range(len(counts)):
+			if counts[i] > 0:
+				print (col, i, counts[i])
